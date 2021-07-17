7db00c0d91be32d9222b2c52f6ca4819be6c09ba
Eoghan <eoghan@hulbert.info>
2021-07-17 09:54:43 -0700

    Modified programming

--- Python/problem10.py
+++ Python/problem10.py
@@ -1,26 +1,27 @@
-# Project Euler Archive Practice
-# https://projecteuler.net/problem=10
+"""
+Project Euler Archive Practice
+https://projecteuler.net/problem=10
 
-# Problem 10 - Summation of Primes
-# SOLVED & VERIFIED VIA PROJECT EULER
+Problem 10 - Summation of Primes
+SOLVED & VERIFIED VIA PROJECT EULER
 
-# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17
-# Find the sum of all the primes below two million
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17
+Find the sum of all the primes below two million
 
-# NOTE: Unable to test for 1m or 2m output due to time to process
-#       the calculations - assuming based on correct ouput for the
-#       limits of 1000, 10000, 100000 that this program functions
-#       as expected to find and calculate correct sums of the primes.
-#
-#       May be possible to enhance the program for optimum processing
-#       to be able to calculate for larger sums without the need of
-#       higher CPU capacity.
-#
-#       Total time to compute sum all primes below 100000: 22 seconds
+NOTE: Unable to test for 1m or 2m output due to time to process
+      the calculations - assuming based on correct ouput for the
+      limits of 1000, 10000, 100000 that this program functions
+      as expected to find and calculate correct sums of the primes.
+
+      May be possible to enhance the program for optimum processing
+      to be able to calculate for larger sums without the need of
+      higher CPU capacity.
+
+      Total time to compute sum all primes below 100000: 22 seconds
+"""
 
 def sumOfPrimes(count):
 
-    primes_List = [] # Create blank list to store prime numbers
     primes = 0 # Create blank variable to sum all prime numbers
     n = 2 # Set variable to iterate and find the prime numbers
 
@@ -34,4 +35,4 @@ def sumOfPrimes(count):
 
     return print(primes) # Print the sum of the primes
 
-sumOfPrimes(100000) # Call function
+sumOfPrimes(1000) # Call functionß
